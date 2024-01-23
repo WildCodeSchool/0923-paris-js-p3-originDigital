e6eb3b4aa091a73cf16650c5c0cbd8c1cddecc66
Alex Kandel <alex.kandel@wanadoo.fr>
2024-01-23 16:55:17 +0100

    added log out button on registered & desktop only

--- frontend/src/pages/Videos/Videos.jsx
+++ frontend/src/pages/Videos/Videos.jsx
@@ -1,12 +1,13 @@
 import Header from "../../components/Header/Header";
+import WatchingVideoCard from "../../components/Watch/Video/WatchingVideoCard";
 import "./Video.css";
 
 function Videos() {
   return (
     <main>
+      <Header />
       <div className="containeur_Body_Video">
-        <Header />
-        <h1>Video page</h1>
+        <WatchingVideoCard />
       </div>
     </main>
   );
